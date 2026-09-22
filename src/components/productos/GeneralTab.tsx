dbb35f44d2caa04c7c26c8b3ddd3fd4ffbd0a83d
kevin-campo <campokevin90@gmail.com>
2026-09-21 19:43:45 -0500

    fix: restaurar imports completos en GeneralTab

--- src/components/productos/GeneralTab.tsx
+++ src/components/productos/GeneralTab.tsx
@@ -1,4 +1,4 @@
-import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
